dbca358e19125c6f46c8c48bae75a38b05b3353f
Vinicius Mesquita <89542212+vsmesquita@users.noreply.github.com>
2024-10-20 20:29:31 -0300

    segundo commit

---
+++ templates/header.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once("config/url.php");
+include_once("config/connection.php");
+include_once("config/process.php");
+
+//LIMPA A MENSSAGEM
+
+if(isset($_SESSION['msg'])){
+
+    $printMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+
+    <!-- BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONTE AWESSOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+
+
+</head>
+
+<body>
+
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Adicionar Contatos</a>
+                </div>
+            </div>
+        </nav>
+    </header>
